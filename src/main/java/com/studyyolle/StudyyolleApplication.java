95669b6c78a3e702cc697eaef9b0641de9d933a2
hwa-yeon <74399187+hwa-yeon@users.noreply.github.com>
2024-03-09 16:57:09 +0900

    first commit

---
+++ src/main/java/com/studyyolle/StudyyolleApplication.java
@@ -0,0 +1,13 @@
+package com.studyyolle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyyolleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyyolleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/studyyolle/StudyyolleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studyyolle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyyolleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
