abe4f6dcf9d8514cd3f9f3800308087e75a57732
hwa-yeon <74399187+hwa-yeon@users.noreply.github.com>
2024-03-10 05:18:37 +0900

    feat: 회원가입 뷰

--- src/main/java/com/studyyolle/config/SecurityConfig.java
+++ src/main/java/com/studyyolle/config/SecurityConfig.java
@@ -1,10 +1,12 @@
 package com.studyyolle.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -22,5 +24,12 @@ public class SecurityConfig {
                 .anyRequest().authenticated();
 
         return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring()
+                .requestMatchers("/node_modules/**")
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
     }
 }
