d7059bb5c8fb1f8f2eac37d2538404dc861a213f
hwa-yeon <74399187+hwa-yeon@users.noreply.github.com>
2024-03-10 04:04:44 +0900

    feat: 회원가입 컨트롤러

---
+++ src/main/java/com/studyyolle/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.studyyolle.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .requestMatchers("/", "/login", "/sign-up", "/check-email", "/check-email-token",
+                        "/email-login", "/check-email-login", "/login-link")
+                .permitAll()
+                .requestMatchers(HttpMethod.GET, "/profile/*").permitAll()
+                .requestMatchers("/css/**, /js/**, /images/**").permitAll()
+                .anyRequest().authenticated();
+
+        return http.build();
+    }
+}
