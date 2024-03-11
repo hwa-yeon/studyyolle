a3665cd1e81eb941f45f7177f9e49c73949c9bf7
hwa-yeon <74399187+hwa-yeon@users.noreply.github.com>
2024-03-12 04:56:48 +0900

    refactor: 회원가입 리팩토링

--- src/main/java/com/studyyolle/account/AccountController.java
+++ src/main/java/com/studyyolle/account/AccountController.java
@@ -1,9 +1,6 @@
 package com.studyyolle.account;
 
-import com.studyyolle.domain.Account;
 import lombok.RequiredArgsConstructor;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -19,8 +16,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class AccountController {
 
     private final SignUpFormValidator signUpFormValidator;
-    private final AccountRepository accountRepository;
-    private final JavaMailSender javaMailSender;
+    private final AccountService accountService;
 
     @InitBinder("signUpForm")
     public void initBinder(WebDataBinder webDataBinder) {
@@ -40,24 +36,7 @@ public class AccountController {
             return "account/sign-up";
         }
 
-        Account account = Account.builder()
-                .email(signUpForm.getEmail())
-                .nickname(signUpForm.getNickname())
-                .password(signUpForm.getPassword()) // TODO password encoding
-                .studyCreatedByWeb(true)
-                .studyUpdatedByWeb(true)
-                .studyEnrollResultByWeb(true)
-                .build();
-
-        Account newAccount = accountRepository.save(account);
-
-        newAccount.generateEmailCheckToken();
-        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
-        simpleMailMessage.setTo(newAccount.getEmail());
-        simpleMailMessage.setSubject("스터디올래, 회원가입 인증");
-        simpleMailMessage.setText("/check-email-token?token=" + newAccount.getEmailCheckToken()
-                                +"&email=" + newAccount.getEmail());
-        javaMailSender.send(simpleMailMessage);
+        accountService.processNewAccount(signUpForm);
 
         return "redirect:/";
     }
---
+++ src/main/java/com/studyyolle/account/AccountService.java
@@ -0,0 +1,44 @@
+package com.studyyolle.account;
+
+import com.studyyolle.domain.Account;
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AccountService {
+
+    private final AccountRepository accountRepository;
+    private final JavaMailSender javaMailSender;
+
+    public void processNewAccount(SignUpForm signUpForm) {
+
+        Account newAccount = saveNewAccount(signUpForm);
+        newAccount.generateEmailCheckToken();
+        sendSignUpConfirmEmail(newAccount);
+    }
+
+    private Account saveNewAccount(SignUpForm signUpForm) {
+        Account account = Account.builder()
+                .email(signUpForm.getEmail())
+                .nickname(signUpForm.getNickname())
+                .password(signUpForm.getPassword()) // TODO password encoding
+                .studyCreatedByWeb(true)
+                .studyUpdatedByWeb(true)
+                .studyEnrollResultByWeb(true)
+                .build();
+
+        return accountRepository.save(account);
+    }
+
+    private void sendSignUpConfirmEmail(Account newAccount) {
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        simpleMailMessage.setTo(newAccount.getEmail());
+        simpleMailMessage.setSubject("스터디올래, 회원가입 인증");
+        simpleMailMessage.setText("/check-email-token?token=" + newAccount.getEmailCheckToken()
+                +"&email=" + newAccount.getEmail());
+        javaMailSender.send(simpleMailMessage);
+    }
+}
